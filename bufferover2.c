4c4edb82df732e617cbf42dbc431d165670dfc3a
iiDeRaR <149298129+iiDeRaR@users.noreply.github.com>
2025-12-26 18:28:37 +0200

    Add files via upload

---
+++ bufferover2.c
@@ -0,0 +1,16 @@
+// buffover2.c
+#include <stdio.h>
+int foo();
+int main() {
+while(1) foo();
+}
+int foo(){
+unsigned int yy = 0;
+char buffer[5]; char ch; int i = 0;
+printf("Say something: ");
+while ((ch = getchar()) != '\n') buffer[i++] = ch;
+buffer[i] = '\0';
+printf("You said: %s\n", buffer);
+printf("The variable yy: %d\n", yy);
+return 0;
+}
